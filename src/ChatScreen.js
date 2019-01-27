17636cde01aa59c1005746e689480f224a975f3b
Violet Wei <zhenzhou.wei@mail.mcgill.ca>
2019-01-27 10:42:55 -0500

    Add files via upload

---
+++ src/ChatScreen.js
@@ -0,0 +1,138 @@
+import React, { Component } from 'react'
+import Chatkit from '@pusher/chatkit-client'
+import MessageList from './components/MessageList'
+import SendMessageForm from './components/SendMessageForm'
+import TypingIndicator from './components/TypingIndicator'
+import WhosOnlineList from './components/WhosOnlineList'
+
+class ChatScreen extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            currentUser: {},
+            currentRoom: {},
+            messages: [],
+            usersWhoAreTyping: [],
+        }
+        this.sendMessage = this.sendMessage.bind(this)
+        this.sendTypingEvent = this.sendTypingEvent.bind(this)
+    }
+
+    sendTypingEvent() {
+        this.state.currentUser
+            .isTypingIn({ roomId: this.state.currentRoom.id })
+            .catch(error => console.error('error', error))    
+    }
+
+    sendMessage(text) {
+        this.state.currentUser.sendMessage({
+            text,
+            roomId: this.state.currentRoom.id,
+        })
+    }
+    componentDidMount () {
+        const chatManager = new Chatkit.ChatManager({
+            instanceLocator: 'v1:us1:1a644bd4-a40e-48e4-acb6-93800217106c',
+            userId: this.props.currentUsername,
+            tokenProvider: new Chatkit.TokenProvider({
+                url: 'http://localhost:3001/authenticate',
+            }),
+        })
+
+        chatManager
+            .connect()
+            .then(currentUser => {
+                this.setState({ currentUser })
+                return currentUser.subscribeToRoom({
+                    roomId: "19398285",
+                    messageLimit: 100,
+                    hooks: {
+                        onMessage: message => {
+                            this.setState({
+                                messages: [...this.state.messages, message],
+                            })
+                        },
+                        onUserStartedTyping: user => {
+                            this.setState({
+                                usersWhoAreTyping: [...this.state.usersWhoAreTyping, user.name],
+                            })
+                        },
+                        onUserStoppedTyping: user => {
+                            this.setState({
+                                usersWhoAreTyping: user => {
+                                    this.setState({
+                                        usersWhoAreTyping: this.state.usersWhoAreTyping.filter(
+                                            username => username !== user.name
+                                        ),
+                                    })
+                                },
+                                onPresenceChange: () => this.forceUpdate(),
+                                onUserJoined: () => this.forceUpdate(),
+                            })
+                        },
+                            
+                    },   
+                })    
+            })
+            .then(currentRoom => {
+                this.setState({ currentRoom })
+            })
+            .catch(error => console.error('error', error))
+    }
+
+    render() {
+        const styles = {
+            container: {
+                height: '100vh',
+                display: 'flex',
+                flexDirection: 'column',
+            },
+            chatContainer: {
+                display: 'flex',
+                flex: 1,
+            },
+            whosOnlineListContainer: {
+                width: '300px',
+                flex: 'none',
+                padding: 20,
+                backgroundColor: '#8A2BE2',
+                color: 'black',
+            },
+            chatListContainer:{
+                padding: 20,
+                width: '85%',
+                display: 'flex',
+                flexDirection: 'column',
+            },
+        }
+
+        return (
+            <div style={styles.container}>
+                <header style={styles.header}>
+                    <h2>Let's Chat</h2>
+                </header>
+                <div style={styles.chatContainer}>
+                    <aside style={styles.whosOnlineListContainer}>
+                        <WhosOnlineList
+                            currentUser={this.state.currentUser}
+                            users={this.state.currentRoom.users}
+                        />    
+                    </aside>
+                    <section style={styles.chatListContainer}>
+                        <MessageList 
+                            messages={this.state.messages}
+                            style={styles.chatList}
+                        />
+                        <TypingIndicator usersWhoAreTyping={this.state.usersWhoAreTyping} />
+                        <SendMessageForm 
+                            onSubmit={this.sendMessage} 
+                            onChange={this.sendTypingEvent}
+                        />
+                    </section>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default ChatScreen
